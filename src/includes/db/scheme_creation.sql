c48010a5fe701e55da2afad4d67bd7a66a1a855a
Ivan <ivaher02@ucm.es>
2022-03-16 17:25:38 +0100

    A test

--- src/includes/db/scheme_creation.sql
+++ src/includes/db/scheme_creation.sql
@@ -5,5 +5,5 @@ create database if not exists easyrent_db
     character set utf8mb4 
     collate utf8mb4_0900_ai_ci;
 
--- Select the created database to be used
+-- Select the created database to be used 
 use easyrent_db;
