ef4f8dccdb4e4a193c30103b8274a015c802a011
Luis Egui <misionpage@gmail.com>
2022-03-18 21:44:45 +0100

    Changed collate.

--- src/includes/db/scheme_creation.sql
+++ src/includes/db/scheme_creation.sql
@@ -3,7 +3,7 @@
 -- Create the database/schema
 create database if not exists easyrent_db 
     character set utf8mb4 
-    collate utf8mb4_0900_ai_ci;
+    collate utf8mb4_general_ci;
 
 -- Select the created database to be used 
 use easyrent_db; 
