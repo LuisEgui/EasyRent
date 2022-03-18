b88c3c86c27d119b77994bd456810b9bb6d0db71
Luis Egui <misionpage@gmail.com>
2022-03-18 05:42:56 +0100

    Restored index.php example file

--- src/index.php
+++ src/index.php
@@ -4,60 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestión de archivos</title>
 </head>
 <body>
-    <header>
-      <h1>EasyRent</h1>
-      <img src="images\logo_easyRent.png" width="270" height="130" />
-      <nav>
-        <ul>
-          <li>
-            <a href="index.html">Inicio</a>
-          </li>
-          <li>
-            <a href="detalles.html">Detalles</a>
-          </li>
-          <li>
-            <a href="bocetos.html">Bocetos</a>
-          </li>
-          <li>
-            <a href="planificacion.html">Planificación</a>
-          </li>
-          <li>
-            <a href="miembros.html">Miembros</a>
-          </li>
-          <li>
-            <a href="contacto.html">Contacto</a>
-          </li>
-          <li>
-            <a href="login.php">login</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-
-    <main>
-      <article>
-        <h1>Descripción del proyecto</h1>
-        <p>
-          EasyRent es un negocio dedicado al alquiler de coches de forma online.
-          No precisamos de ninguna sede física, por lo que toda la interacción
-          con los clientes será a través de la página web.
-        </p>
-        <p>
-          EasyRent permitirá a los clientes buscar coches según la localización
-          que deseen para unos determinados días, meses u horas. Además, los
-          clientes podrán configurar la entrega y devolución del vehículo en
-          determinadas localizaciones, sin necesidad de que estas coincidan o,
-          simplemente, dejando el coche aparcado en la calle, dentro de los
-          límites permitidos, y cerrar el alquiler desde su teléfono móvil.
-        </p>
-        <p>
-          En cuánto a la selección del vehículo, EasyRent permite filtrar por
-          modelo de vehículo, combustible, categoría, caja de cambios, número de
-          asientos o accesorios.
-        </p>
-      </article>
-    </main>
+    <p> <a href="login.php"> login</p>
 </body>
 </html>
