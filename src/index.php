3146065321893f7db0e7719428011767adffa4a6
Luis Egui <misionpage@gmail.com>
2022-03-18 06:00:24 +0100

    FIX: Title name for login example

--- src/index.php
+++ src/index.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestión de archivos</title>
+    <title>Página principal</title>
 </head>
 <body>
-    <p> <a href="login.php"> login</p>
+    <p> <a href="login.php">Login</p>
 </body>
 </html>
