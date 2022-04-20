77dfcbdbd4b7a39b0ef927f0ef305f8316e4a7c4
Luis Egui <misionpage@gmail.com>
2022-04-20 02:04:31 +0200

    Updated Vehicle class

--- src/includes/Vehicle.php
+++ src/includes/Vehicle.php
@@ -43,20 +43,20 @@ class Vehicle {
     /**
      * Creates a Vehicle
      * 
-     * @param string $vin Unique vehicle identifier
+     * @param string $vin Unique vehicle identifier (vin = vehicle identification number)
      * @param string $licensePlate Unique vehicle email
      * @param string $model Vehicle model
      * @param string $image Vehicle image
      * @param string $fuelType Vehicle fuel type. Possible values: 'diesel', 'electric-hybrid', 'electric', 'petrol', 'plug-in-hybrid'.
      * @param string $seatCount Vehicle seat count
      * @param string $state Vehicle state. Possible values: 'available', 'unavailable', 'reserved'.
-     * @return Vehicle
+     * @return void
      */
-    public function __construct($vin, $licensePlate, $model, $image, $fuelType, $seatCount, $state = 'available') {
+    public function __construct($vin, $licensePlate, $model, $image = null, $fuelType, $seatCount, $state = 'available') {
         $this->vin = $vin;
         $this->licensePlate = $licensePlate;
         $this->model = $model;
-        $this->image = $image
+        $this->image = $image;
         $this->fuelType = $fuelType;
         $this->seatCount = $seatCount;
         $this->state = $state;
@@ -179,233 +179,6 @@ class Vehicle {
      */
     public function setState($state) {
         $this->state = $state;
-    }
+    }    
 
-    /**
-     * Check if an user has a determined role
-     * 
-     * @param string $role Check if the user has this role.
-     * @return bool
-     * 
-     * public function hasRole($role) {
-     *    return ($this->role === $role) ? true : false;
-     * }
-     */
-    
-
-}
---------------------------------------------------------------------------------------------------
-class Vehiculo{
-    //use MagicProperties;
-
-	private $vin;
-    private $licensePlate;
-    private $model;
-    private $fuelType;
-    private $seatCount;
-    private $state;
-
-    const AVAILABLE = 0;
-    const UNAVAILABLE = 1;
-    const RESERVED = 2;
-
-    const TYPES_STATE = [self::AVAILABLE => 'Disponible', self::UNAVAILABLE => 'No disponible', self::RESERVED => 'Reservado'];
-
-    public static function getStringEnumState($enum){
-        if($enum < sizeof(self::TYPES_STATE) && $enum >= 0){
-            return self::TYPES_STATE[$enum];
-        }
-        else{
-            return null;
-        }
-    }
-
-    const DIESEL = 0;
-    const ELECTRIC_HYBRID = 1;
-    const ELECTRIC = 2;
-    const PETROL = 3;
-    const PLUG_IN_HYBRID = 4;
-
-    /*const TYPES_STATE = [ 'Diesel' => self::DIESEL, self::ELECTRIC_HYBRID => 'Hibrido', self::ELECTRIC => 'Electrico', self::PETROL => 'Gasolina', self::PLUG_IN_HYBRID => 'Hibrido enchufable'];
-
-    public static function getStringEnumState($enum){
-        if($enum < sizeof(self::TYPES_STATE) && $enum >= 0){
-            return self::TYPES_STATE[$enum];
-        }
-        else{
-            return null;
-        }
-    }*/
-
-    public static function crea($vin, $licensePlate, $model, $fuelType, $seatCount)
-    {
-        $vehiculo = new Vehiculo($vin, $licensePlate, $model, $fuelType, $seatCount, self::AVAILABLE);
-        return $vehiculo;
-    }
-
-    public function __construct($vin, $licensePlate, $model, $fuelType, $seatCount, $state){
-        //hay que asegurarse de que las variables enumeradas toman los valores permitidos y eso se hace antes de llamar a la funcion crea()
-        $this->vin = intval($vin);
-        $this->licensePlate = $licensePlate;
-        $this->model = intval($model);
-        $this->fuelType = $fuelType;
-        $this->seatCount = intval($seatCount);
-        $this->state = $state;
-    }
-
-    private static function getVehiculos($opciones = array())
-    { 
-        $result = [];
-        
-        $conn = BD::getInstance()->getConexionBd();
-
-        $query = 'SELECT * FROM Vehicle';
-
-        if(!empty($opciones)){
-            if($array_key_exists('licensePlate', $opciones)){
-                $opciones['licensePlate'] = $conn->real_escape_string($opciones['licensePlate']);
-            }
-            $query .= 'WHERE ';
-            $contadorFiltros = 0;
-            foreach ($opciones as $columna => $valor){
-                if($contadorFiltros == 0){
-                    $query .= $columna.' = '.$valor;
-                }
-                else if($contadorFiltros > 0){
-                    $query .= 'AND '.$columna.' = '.$valor;
-                }
-                $contadorFiltros++;
-            }
-        }
-
-        $rs = $conn->query($query);
-        if ($rs) {
-            while ($fila = $rs->fetch_assoc()) {
-                $result[] = new Vehiculo($fila['vin'], $fila['licensePlate'], $fila['model'], $fila['fuelType'], $fila['seatCount'], $fila['state']);
-            }
-            $rs->free();
-        } else {
-            error_log($conn->error);
-        }
-
-        return $result; 
-    }
-
-    public static function buscaPorFiltros($opciones = array())
-    {
-        return self::getVehiculos($opciones);
-    }
-
-    public static function inserta($vehiculo)
-    {
-        $result = false;
-
-        $conn = BD::getInstance()->getConexionBd();
-        $query = sprintf(
-            "INSERT INTO Vehicle (vin, licensePlate, model, fuelType, seatCount) VALUES (%d, '%s', %d, %d, %d)",
-            $vehiculo->vin,
-            $conn->real_escape_string($vehiculo->licensePlate), 
-            $vehiculo->model,
-            $vehiculo->fuelType,
-            $vehiculo->seatCount
-        );
-
-        $result = $conn->query($query);
-        if (!$result) {
-            error_log($conn->error);
-        } else if ($conn->affected_rows != 1) {
-            error_log(__CLASS__ . ": Se han insertado '$conn->affected_rows' !");
-        }
-        return $result;
-    }
-
-    private static function actualiza($vehiculo)
-    {
-        $result = false;
-
-        $conn = BD::getInstance()->getConexionBd();
-        $query = sprintf(
-            "UPDATE Vehicle SET licensePlate = '%s', model = %d, fuelType = %d, seatCount = %d, state = %d WHERE vin = %d",
-            $conn->real_escape_string($vehiculo->licensePlate), 
-            $vehiculo->model,
-            $vehiculo->fuelType,
-            $vehiculo->seatCount,
-            $vehiculo->state,
-            $vehiculo->vin,
-        );
-        $result = $conn->query($query);
-        if (!$result) {
-            error_log($conn->error);
-        } else if ($conn->affected_rows != 1) {
-            error_log(__CLASS__ . ": Se han actualizado '$conn->affected_rows' !");
-        }
-        return $result;
-    }
-
-    private static function borra($vehiculo)
-    {
-        return self::borraPorVin($vehiculo->vin);
-    }
-
-    private static function borraPorVin($vinVehiculo)
-    {
-        $result = false;
-
-        $conn = BD::getInstance()->getConexionBd();
-        $query = sprintf("DELETE FROM Vehicle WHERE vin = '%d'", intval($vinVehiculo));
-        $result = $conn->query($query);
-        if (!$result) {
-            error_log($conn->error);
-        } else if ($conn->affected_rows != 1) {
-            error_log(__CLASS__ . ": Se han borrado '$conn->affected_rows' !");
-        }
-
-        return $result;
-    }
-
-    public function getVin()
-    {
-        return $this->vin;
-    }
-    public function getLicensePlate()
-    {
-        return $this->licensePlate;
-    }
-    public function getModel()
-    {
-        return $this->model;
-    }
-    public function getFuelType()
-    {
-        return $this->fuelType;
-    }
-    public function getSeatCount()
-    {
-        return $this->seatCount;
-    }
-    public function getState()
-    {
-        return $this->state;
-    }
-
-    public function setLicensePlate($licPlate)
-    {
-        $this->licensePlate = $licPlate;
-    }
-    public function setModel($model)
-    {
-        $this->model = $model;
-    }
-    public function setFuelType($fuelT)
-    {
-        $this->fuelType = $fuelT;
-    }
-    public function setSeatCount($seatC)
-    {
-        $this->seatCount = $seatC;
-    }
-    public function setState($state)
-    {
-        $this->state = $state;
-    }
 }
