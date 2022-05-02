052397945bea6099f7dc765ee81f6f6d49ea5950
Notkero <notkerga@ucm.es>
2022-05-02 02:13:18 +0200

    included form to add message to foro.php

--- src/foro.php
+++ src/foro.php
@@ -4,6 +4,7 @@ require_once __DIR__.'/includes/config.php';
 require_once __DIR__.'/includes/Message.php';
 require_once __DIR__.'/includes/UserService.php';
 require_once __DIR__.'/includes/MessageService.php';
+require_once __DIR__.'/includes/FormularioRegistroMensaje.php';
 
 $tituloPagina = 'Foro';
 
@@ -12,38 +13,50 @@ $messageService = new MessageService($GLOBALS['db_message_repository']);
 $messages = $messageService->readAllMessages();
 $userService = new UserService($GLOBALS['db_user_repository'], $GLOBALS['db_image_repository']);
 
+$contenidoPrincipal .= <<<EOS
+<h1>Lista de mensajes</h1>
+EOS;
+for ($i = 0; $i < count($messages); $i++) {
+$contenidoPrincipal .= <<<EOS
+<div class="v">
+	<h2>Mensaje 
+EOS;
+$contenidoPrincipal .= $i + 1;
+$contenidoPrincipal .= <<<EOS
+	</h2>
+	<p>
+	Autor: 
+EOS;
+$contenidoPrincipal .= $messages[$i]->getAuthor();
+$contenidoPrincipal .= <<<EOS
+	</p>
+	<p>
+	Texto: 
+EOS;
+$contenidoPrincipal .= $messages[$i]->getTxt();
+$contenidoPrincipal .= <<<EOS
+	</p>
+	<p>
+	Fecha: 
+EOS;
+$contenidoPrincipal .= $messages[$i]->getSendTime();
+$contenidoPrincipal .= <<<EOS
+	</p> 
+EOS;
+}
+
 if ($userService->isLogged()) {
+
+	$form = new FormularioRegistroMensaje();
+	$htmlFormRegMessage = $form->gestiona();
+
+	$tituloPagina = 'Publicación de mensaje al foro';
+
 	$contenidoPrincipal .= <<<EOS
-		<h1>Lista de mensajes</h1>
+	<h1>Mensaje</h1>
+	$htmlFormRegMessage
 	EOS;
-	for ($i = 0; $i < count($messages); $i++) {
-		$contenidoPrincipal .= <<<EOS
-		<div class="v">
-			<h2>Mensaje 
-		EOS;
-		$contenidoPrincipal .= $i + 1;
-		$contenidoPrincipal .= <<<EOS
-			</h2>
-			<p>
-			Autor: 
-		EOS;
-		$contenidoPrincipal .= $messages[$i]->getAuthor();
-		$contenidoPrincipal .= <<<EOS
-			</p>
-			<p>
-			Texto: 
-		EOS;
-		$contenidoPrincipal .= $messages[$i]->getTxt();
-		$contenidoPrincipal .= <<<EOS
-			</p>
-			<p>
-			Fecha: 
-		EOS;
-		$contenidoPrincipal .= $messages[$i]->getSendTime();
-		$contenidoPrincipal .= <<<EOS
-			</p> 
-		EOS;
-	}
+
 }
 
 
--- src/includes/FormularioRegistroMensaje.php
+++ src/includes/FormularioRegistroMensaje.php
@@ -5,27 +5,27 @@ require_once __DIR__.'/MessageService.php';
 
 class FormularioRegistroMensaje extends Formulario {
 
-    private $mensajeService;
+    private $messageService;
     
     public function __construct() {
         parent::__construct('formRegisterMessage', ['urlRedireccion' => 'foro.php']);
-        $this->$mensajeService = new MessageService($GLOBALS['db_message_repository'], $GLOBALS['db_image_repository']);
+        $this->$messageService = new MessageService($GLOBALS['db_message_repository']);
     }
     
     protected function generaCamposFormulario(&$datos) {
         // Se reutiliza el email introducido previamente o se deja en blanco
-        $message = $datos['mensaje'] ?? '';
+        $message = $datos['message'] ?? '';
 
         // Se generan los mensajes de error si existen.
         $htmlErroresGlobales = self::generaListaErroresGlobales($this->errores); // Se muestra como una lista
-        $erroresCampos = self::generaErroresCampos(['mensaje'], $this->errores, 'span', array('class' => 'error')); // Agrupa los errores por campos para luego mostrar cada tipo de error donde corresponda haciendo uso d la posicion del array erroresCampos correcta
+        $erroresCampos = self::generaErroresCampos(['message'], $this->errores, 'span', array('class' => 'error')); // Agrupa los errores por campos para luego mostrar cada tipo de error donde corresponda haciendo uso d la posicion del array erroresCampos correcta
         // Se genera el HTML asociado a los campos del formulario y los mensajes de error.
         $html = <<<EOF
         $htmlErroresGlobales
         <fieldset>
             <div>
-                <label for="mensaje">Mensaje:</label>
-                <input id="mensaje" type="text" name="mensaje" value="$message" />{$erroresCampos['mensjae']}
+                <label for="message">Mensaje:</label>
+                <input id="message" type="text" name="message" value="$message" />{$erroresCampos['message']}
             </div>
             <div>
                 <button type="submit" name="register"> Registrar </button>
@@ -37,13 +37,13 @@ class FormularioRegistroMensaje extends Formulario {
 
     protected function procesaFormulario(&$datos) {
         $this->errores = [];
-        $message = trim($datos['mensaje'] ?? '');
+        $message = trim($datos['message'] ?? '');
 
         if ( ! $message || empty($message))
             $this->errores['message'] = 'El mensaje no puede estar vacío';
         
         if (count($this->errores) === 0) {
-            $mensajenuevo = $this->mensajeService->createMessage($message);
+            $newMessage = $this->messageService->createMessage($message);
         
  
             header("Location: {$this->urlRedireccion}");
