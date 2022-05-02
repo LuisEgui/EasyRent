92cebe5ee6c77feca88ee5ac6accffe84e635a47
Notkero <notkerga@ucm.es>
2022-05-02 19:53:54 +0200

    New order to the foro.php messages according to answering order

--- src/foro.php
+++ src/foro.php
@@ -19,56 +19,117 @@ $userService = new UserService($GLOBALS['db_user_repository'], $GLOBALS['db_imag
 $contenidoPrincipal .= <<<EOS
 <h1>Lista de mensajes</h1>
 EOS;
+$numMessagesShown = 0;
 for ($i = 0; $i < count($messages); $i++) {
-$contenidoPrincipal .= <<<EOS
-<div class="v">
-	<h2>Mensaje 
-EOS;
-$contenidoPrincipal .= $i + 1;
-$contenidoPrincipal .= <<<EOS
-	</h2>
-	<p>
-	Autor: 
-EOS;
-$idAuthor = $messages[$i]->getAuthor();
-$userAuthor = $userService->readUserById($idAuthor);
-$contenidoPrincipal .= $userAuthor->getEmail();
-$contenidoPrincipal .= <<<EOS
-	</p>
-	<p>
-	Texto: 
-EOS;
-$contenidoPrincipal .= $messages[$i]->getTxt();
-$contenidoPrincipal .= <<<EOS
-	</p>
-	<p>
-	Fecha: 
-EOS;
-$contenidoPrincipal .= $messages[$i]->getSendTime();
-$contenidoPrincipal .= <<<EOS
-	</p> 
-EOS;
 
-if ($userService->isLogged()) {
-	$formres = new FormularioResponderMensaje($messages[$i]->getId());
-	$htmlFormResMessage = $formres->gestiona();
-	$contenidoPrincipal .= <<<EOS
-	$htmlFormResMessage
-	EOS;
-	if($userService->readUserByEmail($_SESSION['email'])->getId() == $messages[$i]->getAuthor()){
-		$formdel = new FormularioEliminarMensaje($messages[$i]->getId());
-		$htmlFormDelMessage = $formdel->gestiona();
-
-		//PARA EDITAR MENSAJE
-		$formact = new FormularioActualizarMensaje($messages[$i]->getId());
-		$htmlFormActMessage = $formact->gestiona();
+	if($messages[$i]->getIdParentMessage() == null){
 		$contenidoPrincipal .= <<<EOS
-		$htmlFormDelMessage
-		$htmlFormActMessage
+		<div class="v">
+			<h2>Mensaje 
 		EOS;
+		$contenidoPrincipal .= $i + 1;
+		$contenidoPrincipal .= <<<EOS
+			</h2>
+			<p>
+			Autor: 
+		EOS;
+		$idAuthor = $messages[$i]->getAuthor();
+		$userAuthor = $userService->readUserById($idAuthor);
+		$contenidoPrincipal .= $userAuthor->getEmail();
+		$contenidoPrincipal .= <<<EOS
+			</p>
+			<p>
+			Texto: 
+		EOS;
+		$contenidoPrincipal .= $messages[$i]->getTxt();
+		$contenidoPrincipal .= <<<EOS
+			</p>
+			<p>
+			Fecha: 
+		EOS;
+		$contenidoPrincipal .= $messages[$i]->getSendTime();
+		$contenidoPrincipal .= <<<EOS
+			</p> 
+		EOS;
+	
+		if ($userService->isLogged()) {
+			$formres = new FormularioResponderMensaje($messages[$i]->getId());
+			$htmlFormResMessage = $formres->gestiona();
+			$contenidoPrincipal .= <<<EOS
+			$htmlFormResMessage
+			EOS;
+			if($userService->readUserByEmail($_SESSION['email'])->getId() == $messages[$i]->getAuthor()){
+				$formdel = new FormularioEliminarMensaje($messages[$i]->getId());
+				$htmlFormDelMessage = $formdel->gestiona();
+	
+				//PARA EDITAR MENSAJE
+				$formact = new FormularioActualizarMensaje($messages[$i]->getId());
+				$htmlFormActMessage = $formact->gestiona();
+				$contenidoPrincipal .= <<<EOS
+				$htmlFormDelMessage
+				$htmlFormActMessage
+				EOS;
+				
+			} 
+		}
+		$numMessagesShown++;
+	}
+
+	for($j = $i + 1; $j < count($messages); $j++){
+
+		if($messages[$j]->getIdParentMessage() == $messages[$i]->getId() && $numMessagesShown <= $j){
+			$contenidoPrincipal .= <<<EOS
+			<div class="v">
+				<h2>Mensaje 
+			EOS;
+			$contenidoPrincipal .= $j + 1;
+			$contenidoPrincipal .= <<<EOS
+				</h2>
+				<p>
+				Autor: 
+			EOS;
+			$idAuthor = $messages[$j]->getAuthor();
+			$userAuthor = $userService->readUserById($idAuthor);
+			$contenidoPrincipal .= $userAuthor->getEmail();
+			$contenidoPrincipal .= <<<EOS
+				</p>
+				<p>
+				Texto: 
+			EOS;
+			$contenidoPrincipal .= $messages[$j]->getTxt();
+			$contenidoPrincipal .= <<<EOS
+				</p>
+				<p>
+				Fecha: 
+			EOS;
+			$contenidoPrincipal .= $messages[$j]->getSendTime();
+			$contenidoPrincipal .= <<<EOS
+				</p> 
+			EOS;
 		
-	} 
-}
+			if ($userService->isLogged()) {
+				$formres = new FormularioResponderMensaje($messages[$j]->getId());
+				$htmlFormResMessage = $formres->gestiona();
+				$contenidoPrincipal .= <<<EOS
+				$htmlFormResMessage
+				EOS;
+				if($userService->readUserByEmail($_SESSION['email'])->getId() == $messages[$j]->getAuthor()){
+					$formdel = new FormularioEliminarMensaje($messages[$j]->getId());
+					$htmlFormDelMessage = $formdel->gestiona();
+		
+					//PARA EDITAR MENSAJE
+					$formact = new FormularioActualizarMensaje($messages[$j]->getId());
+					$htmlFormActMessage = $formact->gestiona();
+					$contenidoPrincipal .= <<<EOS
+					$htmlFormDelMessage
+					$htmlFormActMessage
+					EOS;
+					
+				} 
+			}	
+			$numMessagesShown++;
+		}
+	}
 }
 
 if ($userService->isLogged()) {
