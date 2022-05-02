37bb7b3b2f1656403ccc9630ea7b4c85aaee85d6
Triciabars <pplata@ucm.es>
2022-05-02 12:47:56 +0200

    crea mensaje

--- src/includes/FormularioRegistroMensaje.php
+++ src/includes/FormularioRegistroMensaje.php
@@ -14,18 +14,18 @@ class FormularioRegistroMensaje extends Formulario {
     
     protected function generaCamposFormulario(&$datos) {
         // Se reutiliza el email introducido previamente o se deja en blanco
-        $message = $datos['message'] ?? '';
+        $message = $datos['txt'] ?? '';
 
         // Se generan los mensajes de error si existen.
         $htmlErroresGlobales = self::generaListaErroresGlobales($this->errores); // Se muestra como una lista
-        $erroresCampos = self::generaErroresCampos(['message'], $this->errores, 'span', array('class' => 'error')); // Agrupa los errores por campos para luego mostrar cada tipo de error donde corresponda haciendo uso d la posicion del array erroresCampos correcta
+        $erroresCampos = self::generaErroresCampos(['txt'], $this->errores, 'span', array('class' => 'error')); // Agrupa los errores por campos para luego mostrar cada tipo de error donde corresponda haciendo uso d la posicion del array erroresCampos correcta
         // Se genera el HTML asociado a los campos del formulario y los mensajes de error.
         $html = <<<EOF
         $htmlErroresGlobales
         <fieldset>
             <div>
-                <label for="message">Mensaje:</label>
-                <input id="message" type="text" name="message" value="$message" />{$erroresCampos['message']}
+                <label for="txt">Mensaje:</label>
+                <input id="txt" type="text" name="txt" value="$txt" />{$erroresCampos['txt']}
             </div>
             <div>
                 <button type="submit" name="register"> Registrar </button>
