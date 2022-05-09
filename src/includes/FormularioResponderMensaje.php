a1732fbb7b1a05acd3807ba3a50defd774c82a37
Notkero <notkerga@ucm.es>
2022-05-09 21:02:43 +0200

    issues with answering messages' id solved

--- src/includes/FormularioResponderMensaje.php
+++ src/includes/FormularioResponderMensaje.php
@@ -8,11 +8,13 @@ class FormularioResponderMensaje extends Formulario {
 
     private $messageService;
     private $userService;
+    private $messageId;
     
     public function __construct($id) {
         parent::__construct('formAnswerMessage?id=' . $id, ['urlRedireccion' => 'foro.php']);
         $this->messageService = new MessageService($GLOBALS['db_message_repository']);
         $this->userService = new UserService($GLOBALS['db_user_repository'], $GLOBALS['db_image_repository']);
+        $this->messageId = $id;
     }
     
     protected function generaCamposFormulario(&$datos) {
@@ -40,7 +42,7 @@ class FormularioResponderMensaje extends Formulario {
             $this->errores['txt'] = 'El mensaje no puede estar vacío';
         
         if (count($this->errores) === 0) {
-            $newMessage = $this->messageService->createMessage($txt, $author->getId(), $id);
+            $newMessage = $this->messageService->createMessage($txt, $author->getId(), $this->messageId);
 
         if ($newMessage === null)
             $this->errores['txt'] = 'No se ha podido agregar el mensaje!';
