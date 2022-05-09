7e4300cbda424b10303f63e0a039b053891f1b6e
Notkero <notkerga@ucm.es>
2022-05-09 20:16:21 +0200

    Update on message's update functioning as expected

--- src/includes/FormularioActualizarMensaje.php
+++ src/includes/FormularioActualizarMensaje.php
@@ -8,19 +8,20 @@ class FormularioActualizarMensaje extends Formulario {
 
     private $messageService;
     private $userService;
-    
-    public function __construct() {
-        parent::__construct('formEditMessage', ['urlRedireccion' => 'foro.php']);
+    private $messageId;
+
+    public function __construct($id) {
+        parent::__construct('formEditMessage' . $id, ['urlRedireccion' => 'foro.php']);
         $this->messageService = new MessageService($GLOBALS['db_message_repository']);
         $this->userService = new UserService($GLOBALS['db_user_repository'], $GLOBALS['db_image_repository']);
+        $this->messageId = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
     }
     
     protected function generaCamposFormulario(&$datos) {
-        $mensajes = $this->messageService->readAllMessages();
 
         // Se reutiliza el email introducido previamente o se deja en blanco
         $mensaje = $datos['txt'] ?? '';
-
+        $mensaje = $this->messageService->readMessageById($this->messageId)->getTxt();
         // Se generan los mensajes de error si existen.
         $htmlErroresGlobales = self::generaListaErroresGlobales($this->errores); // Se muestra como una lista
         // Agrupa los errores por campos para luego mostrar cada tipo de error donde corresponda haciendo uso d la
@@ -28,38 +29,13 @@ class FormularioActualizarMensaje extends Formulario {
         $erroresCampos = self::generaErroresCampos(['txt', 'seleccionMensaje'], $this->errores, 'span', array('class' => 'error'));
         $html = <<<EOS
         $htmlErroresGlobales
-            <div>
-            <table>
-                <tr>
-                    <th></th>
-                    <th>Autor  </th>
-                    <th>Fecha  </th>
-                    <th>Texto  </th>
-                </tr>
-            </table>
-            </div>
         EOS;
 
-        foreach($mensajes as $message) {
-
-            $idAuthor = $message->getAuthor();
-            $userAuthor = $this->userService->readUserById($idAuthor);
-            if($userAuthor->getEmail() == $_SESSION['email']){
-            $html .= <<<EOS
-                <tr>
-                    <td><input type="radio" name="seleccionMensaje" value="{$message->getId()} required"></td>
-                    <td>{$userAuthor->getEmail()}</td>
-                    <td>{$message->getSendTime()}</td>
-                    <td>{$message->getTxt()}</td>
-                </tr>
-            EOS;
-            }
-        }
         $html .= <<<EOS
         <fieldset>
             <div>
                 <label for="txt">Mensaje:</label>
-                <input id="txt" type="text" name="txt" value="$mensaje" />{$erroresCampos['txt']}
+                <input id="txt" type="text" name="txt" value="$mensaje" style="width : 600px; heigth : 600px"/>{$erroresCampos['txt']}
             </div>
             <div>
                 <button type="submit" name="answer"> Actualizar </button>
@@ -72,16 +48,15 @@ class FormularioActualizarMensaje extends Formulario {
     protected function procesaFormulario(&$datos) {            
         $this->errores = [];
         $txt = trim($datos['txt'] ?? '');
-        $idMessage = $datos['seleccionMensaje'];
         $author = $this->userService->readUserByEmail($_SESSION['email']);
 
         if (empty($txt))
             $this->errores['txt'] = 'El mensaje no puede estar vacío';
-        if (empty($idMessage))
+        if (empty($this->messageId))
         $this->errores['seleccionMensaje'] = 'Debe seleccionar una opción';
         
         if (count($this->errores) === 0) {
-            $edMessage = $this->messageService->updateMessage($txt, $idMessage);
+            $edMessage = $this->messageService->updateMessage($txt, $this->messageId);
 
         if ($edMessage === null)
             $this->errores['txt'] = 'No se ha podido agregar el mensaje!';
